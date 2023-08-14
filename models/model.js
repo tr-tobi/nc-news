5e79cff6f97f560c68705136daa52a4b5ef9a1a6
Tobi Thomas <tobi@tobis-air.home>
2023-08-14 18:09:38 +0100

    selects data from table that corresponds with the given id

--- models/model.js
+++ models/model.js
@@ -3,3 +3,11 @@ const db = require("../db/connection");
 exports.sendTopics = () => {
   return db.query("SELECT * FROM topics").then((result) => result.rows);
 };
+
+exports.selectArticleById = (article_id) => {
+  return db
+    .query("SELECT * FROM articles WHERE article_id = $1;", [article_id])
+    .then((result) => {
+      return result.rows[0];
+    });
+};
