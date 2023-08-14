5e9f1835adbf51dabf426efbbef9ac0089910758
Tobi Thomas <tobi@tobis-air.home>
2023-08-14 13:57:11 +0100

    removes first two assertions

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const { getTopics } = require("./controllers/controller");
 
 const app = express();
-app.use(express.json());
 
 app.get("/api/topics", getTopics);
 
