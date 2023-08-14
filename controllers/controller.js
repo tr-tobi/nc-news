a4652b699272e554886b9680f27ea287a378a7b2
Tobi Thomas <tobi@tobis-air.home>
2023-08-14 18:12:45 +0100

    creates getArticleByid which handles responses for 200 code and 404 error code

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,4 @@
-const { sendTopics } = require("../models/model");
+const { sendTopics, selectArticleById } = require("../models/model");
 const endpoints = require("../endpoints.json");
 
 exports.getEndpoints = (req, res) => {
@@ -6,9 +6,20 @@ exports.getEndpoints = (req, res) => {
 };
 
 exports.getTopics = (req, res) => {
-  sendTopics()
-    .then((topic) => {
-      res.status(200).send({ topic });
+  sendTopics().then((topic) => {
+    res.status(200).send({ topic });
+  });
+};
+
+exports.getArticleByid = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleById(article_id)
+    .then((article) => {
+      if (article !== undefined) {
+        res.status(200).send({ article });
+      } else {
+        res.status(404).send({ msg: "Article does not exist" });
+      }
     })
     .catch((err) => {
       next(err);
