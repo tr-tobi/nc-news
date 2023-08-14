ca42c560d8cf9f9537f2d107ddf36093c003b2cd
Tobi Thomas <tobi@tobis-air.home>
2023-08-14 18:04:53 +0100

    creates response to GET request for /articles/:article_id

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { getTopics, getEndpoints } = require("./controllers/controller");
+const {
+  getTopics,
+  getEndpoints,
+  getArticleByid,
+} = require("./controllers/controller");
+const { handle400Errors } = require("./controllers/errors.controller");
 
 const app = express();
 
 app.get("/api", getEndpoints);
-
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleByid);
+
+app.use(handle400Errors);
 
 app.use((err, req, res, next) => {
   res.status(500).send({ msg: "Internal Server Error" });
