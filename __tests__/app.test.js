ee8926ed5fdd0bd31c015c59f250bce951a176e6
Tobi Thomas <tobi@tobis-air.home>
2023-08-14 18:14:31 +0100

    implements testing for the success and errors of /articles/:article_id endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -35,3 +35,39 @@ describe("/topic", () => {
       });
   });
 });
+
+describe("/articles/:article_id", () => {
+  test("GET:200 sends an object containing a single article to the end point ", () => {
+    return req(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((res) => {
+        expect(res.body.article).toMatchObject({
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  test("GET:404 sends an appropriate and error message when given a valid but non-existent id", () => {
+    return req(app)
+      .get("/api/articles/999")
+      .expect(404)
+      .then((res) => {
+        expect(res.body.msg).toBe("Article does not exist");
+      });
+  });
+  test("GET:400 sends an appropriate and error message when given an invalid id", () => {
+    return req(app)
+      .get("/api/articles/hello")
+      .expect(400)
+      .then((res) => {
+        expect(res.body.msg).toBe("Invalid id");
+      });
+  });
+});
