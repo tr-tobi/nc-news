134becb891e99d94ead7ee025c89319520d30e4d
Tobi Thomas <tobi@tobis-air.home>
2023-08-15 11:24:42 +0100

    handles custom error for articles that dont exist

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -5,3 +5,11 @@ exports.handle400Errors = (err, req, res, next) => {
     next(err);
   }
 };
+
+exports.handleCustomErrors = (err, req, res, next) => {
+  if (err.status === 404) {
+    res.status(404).send({ msg: "Article does not exist" });
+  } else {
+    next(err);
+  }
+};
