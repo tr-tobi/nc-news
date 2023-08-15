6b48770f8e195a92f61a49625fdb57280b81a1ec
Tobi Thomas <tobi@tobis-air.home>
2023-08-15 14:08:38 +0100

    queries for articles and for comment count

--- models/model.js
+++ models/model.js
@@ -1,7 +1,24 @@
 const db = require("../db/connection");
+const { formatComments } = require("../db/seeds/utils");
 
 exports.sendTopics = () => {
   return db.query("SELECT * FROM topics").then((result) => result.rows);
+};
+
+exports.sendArticles = () => {
+  const articles = db
+    .query("SELECT * FROM articles ORDER BY created_at DESC")
+    .then((result) => result.rows);
+  const commentCount = db
+    .query(
+      `SELECT COUNT(*)
+    AS comment_count
+    FROM articles
+    GROUP BY article_id
+    ORDER BY created_at DESC`
+    )
+    .then((result) => result.rows);
+  return Promise.all([articles, commentCount]);
 };
 
 exports.selectArticleById = (article_id) => {
