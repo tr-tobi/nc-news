3ae916c6057ca8ee2ebe7cd4e631acb25450d82d
Tobi Thomas <tobi@tobis-air.home>
2023-08-17 12:49:09 +0100

    handles errors and custom erros

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,6 +1,8 @@
 exports.handle400Errors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid id" });
+  } else if (err.code === "23502" && err.detail.includes("Failing")) {
+    res.status(400).send({ msg: "Bad Request" });
   } else {
     next(err);
   }
