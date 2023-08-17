a8c092f88a0ecfa00798039f6d8e6df8cabbad79
Tobi Thomas <tobi@tobis-air.home>
2023-08-17 01:16:26 +0100

    handles 400 errors and custom errors

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,13 +1,18 @@
 exports.handle400Errors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid id" });
+  } else if (err.code === "23503" && err.detail.includes("author")) {
+    res.status(400).send({ msg: "Invalid Username" });
   } else {
     next(err);
   }
 };
 
 exports.handleCustomErrors = (err, req, res, next) => {
-  if (err.status === 404) {
+  if (
+    err.status === 404 ||
+    (err.code === "23503" && err.detail.includes("article_id"))
+  ) {
     res.status(404).send({ msg: "Article does not exist" });
   } else {
     next(err);
