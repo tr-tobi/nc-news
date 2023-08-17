5b114a2a0a532e3495c23c3d7350e3d09a06e218
Tobi Thomas <tobi@tobis-air.home>
2023-08-17 16:00:32 +0100

    implements testing for users endpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -20,7 +20,45 @@ describe("/", () => {
   });
 });
 
-describe("/topic", () => {
+describe("/users", () => {
+  test("GET:200 sends an array of user objects ", () => {
+    return req(app)
+      .get("/api/users")
+      .expect(200)
+      .then((res) => {
+        expect(res.body).toEqual({
+          users: [
+            {
+              username: "butter_bridge",
+              name: "jonny",
+              avatar_url:
+                "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+            },
+            {
+              username: "icellusedkars",
+              name: "sam",
+              avatar_url:
+                "https://avatars2.githubusercontent.com/u/24604688?s=460&v=4",
+            },
+            {
+              username: "rogersop",
+              name: "paul",
+              avatar_url:
+                "https://avatars2.githubusercontent.com/u/24394918?s=400&v=4",
+            },
+            {
+              username: "lurker",
+              name: "do_nothing",
+              avatar_url:
+                "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+            },
+          ],
+        });
+      });
+  });
+});
+
+describe("/topics", () => {
   test("GET:200 sends an array of the topics to the end point ", () => {
     return req(app)
       .get("/api/topics")
