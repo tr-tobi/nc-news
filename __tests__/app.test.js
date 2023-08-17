e39b79e4e1f40130f1d5b7f6170f94c9b215d1fd
Tobi Thomas <tobi@tobis-air.home>
2023-08-17 22:12:56 +0100

    implements testing for comment_count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -190,6 +190,7 @@ describe("/articles/:article_id", () => {
           votes: 100,
           article_img_url:
             "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: 11,
         });
         expect(res.body.article.article_id).toEqual(1);
       });
