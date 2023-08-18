721653f4909e05ec138d81d7f8824f03ed14c2d9
Tobi Thomas <tobi@tobis-air.home>
2023-08-18 10:07:44 +0100

    handles error responses

--- controllers/errors.controller.js
+++ controllers/errors.controller.js
@@ -1,15 +1,17 @@
 exports.handle400Errors = (err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid id" });
-  } else if (err.code === "23502") {
-    res.status(400).send({ msg: "Bad Request" });
-  } else if (err.code === "23503" && err.detail.includes("author")) {
-    res.status(400).send({ msg: "Invalid Username" });
   } else if (
-    (err.code === "23502" && err.detail.includes("Failing")) ||
+    err.code === "23502" ||
     (err.status === 400 && err.msg === "Bad Request")
   ) {
     res.status(400).send({ msg: "Bad Request" });
+  } else if (err.code === "23503" && err.detail.includes("author")) {
+    res.status(400).send({ msg: "Invalid Username" });
+  } else if (err.code === "23502" && err.detail.includes("Failing")) {
+    res.status(400).send({ msg: "Bad Request" });
+  } else if (err.status === 404 && err.msg === "Not Found") {
+    res.status(404).send({ msg: "Not Found" });
   } else {
     next(err);
   }
